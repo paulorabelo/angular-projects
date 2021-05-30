8b0f6467a00bf6f892490f9d336536fceea3aa89
Paulo Rabelo <paulorabelo@hotmail.com>
2021-05-29 23:43:39 -0300

    configuração da camada Repository

--- backend/src/main/java/com/project/backend/repository/StockRepository.java
+++ backend/src/main/java/com/project/backend/repository/StockRepository.java
@@ -1,4 +1,9 @@
 package com.project.backend.repository;
 
-public interface StockRepository {
+import com.project.backend.model.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockRepository extends JpaRepository<Stock, Long> {
 }
