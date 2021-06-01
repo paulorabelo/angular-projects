626194e72fb868f5c3bdca9aaee86c0259a2a655
Paulo Rabelo <paulorabelo@hotmail.com>
2021-05-31 23:08:31 -0300

    Modificado varios módulos e adicionado a exceptions-NotFoundException

--- backend/src/main/java/com/project/backend/mapper/StockMapper.java
+++ backend/src/main/java/com/project/backend/mapper/StockMapper.java
@@ -4,6 +4,9 @@ import com.project.backend.model.Stock;
 import com.project.backend.model.dto.StockDTO;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class StockMapper {
     public Stock toEntity(StockDTO dto) {
@@ -24,5 +27,9 @@ public class StockMapper {
         dto.setVariation(stock.getVariation());
         dto.setDate(stock.getDate());
         return dto;
+    }
+
+    public List<StockDTO> toDto(List<Stock> listStock){
+        return listStock.stream().map(this::toDto).collect(Collectors.toList());
     }
 }
